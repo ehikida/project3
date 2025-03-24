56b6b56566e23c70d115002d18ef7bf40912d7b1
Beth Mena <bethhikida@gmail.com>
2025-03-23 23:02:41 -0600

    toggle styles

--- js/index.js
+++ js/index.js
@@ -9,3 +9,32 @@ function openMenu(main_item) {
     document.getElementById("video").style.display = "inherit";
   }
 }
+
+function addCss() {
+  var head = document.head;
+  var link = document.createElement("link");
+
+  link.type = "text/css";
+  link.rel = "stylesheet";
+  link.href = "css/toggle.css";
+  link.id = "style2";
+
+  head.appendChild(link);
+}
+
+function removeCss() {
+  var ele = document.getElementById("style2");
+  ele.remove();
+}
+
+var toggle = 0;
+function toggleStylesheet() {
+  if (toggle == 0) {
+    addCss();
+    toggle = 1;
+  } else {
+    removeCss();
+    toggle = 0;
+  }
+  console.log("clicked!");
+}
